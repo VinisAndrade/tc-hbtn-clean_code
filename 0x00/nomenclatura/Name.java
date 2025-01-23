f193296944cd0802b6f355b309ec3c6ff73afdc0
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-23 15:55:23 -0300

    Update Name.java

--- 0x00/nomenclatura/Name.java
+++ 0x00/nomenclatura/Name.java
@@ -1,48 +1,37 @@
 public class Name {
-
     // Variáveis com nomes significativos
     public Date modifiedDate;
     public List<Testcase> findAllTestcasesByUser(Name.User user) {
         // Implementação
     }
-
     // Declarações corretas
-    public List<Account> accountList;
-
+    public List<Account> AccountList;
     // Métodos sem prefixos desnecessários
     public void deleteUser(Name.User user) {
         // Implementação
     }
-
     // Nomes claros para evitar mapas mentais
     public URL url;
-
     // Classes possuem nomes próprios; funções devem ter verbos
     public class TableRepresenter {
         // Implementação
     }
-
     public void saveCar() {
         // Implementação
     }
-
     // Uma palavra por conceito
     public void saveUser() {
         // Implementação
     }
-
     public void createAccount() {
         // Implementação
     }
-
     public void generateContract() {
         // Implementação
     }
-
     // Use constantes e enums em vez de números mágicos
     public static final int DAYS_IN_YEAR = 360;
     public static final int HOURS_IN_WEEK = 168;
-
     // Classe interna User como exemplo
     public static class User {
         // Implementação
